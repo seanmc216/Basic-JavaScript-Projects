ea6c2f9fe3b02e1476481fdb1ebb15beb9f3eead
Sean Croney <103222228+seanmc216@users.noreply.github.com>
2023-01-31 19:29:43 +0000

    Updated Assignment #3 submission

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,17 +1,17 @@
-// function addition_Function() {
-//     var addition = 2 + 2;
-//     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
-// }
+function addition_Function() { //This targets the element with id "Math" and overwrites Click This!
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
 
-// function subtraction_Function() {
-//     var Subtraction = 5 - 2;
-//     document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
-// }
+function subtraction_Function() { //This targets the element with id "Math2" and overwrites Click This!
+    var Subtraction = 5 - 2;
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + Subtraction;
+}
 
-// function multiplication() {
-//     var simple_Math = 6 * 8;
-//     document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math;
-// }
+function multiplication() { //This targets the element with id "Math3" and overwrites Click This!
+    var simple_Math = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_Math;
+}
 
 // function division() {
 //     var simple_Math = 48 / 6;
@@ -23,27 +23,27 @@
 //     document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 // }
 
-// function modulus_Operator() {
-//     var simple_Math = 25 % 6;
-//     document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
-// }
+function modulus_Operator() { //This targets the element with id "Math4" and overwrites Click This!
+    var simple_Math = 25 % 6;
+    document.getElementById("Math4").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
 
 // function negation_Operator() {
 //     var x = 10;
 //     document.getElementById("Math").innerHTML = -x;
 // }
 
-// function increment_Operator() {
-//     var X = 5;
-//     X++;
-//     document.write(X);
-// }
+function increment_Operator() { //This targets the element with id "Math5" and overwrites Click This!
+    var X = 5;
+    X++;
+    document.getElementById("Math5").innerHTML = (X);
+}
 
-// function decrement_Operator() {
-//     var X = 5.25;
-//     X--;
-//     document.write(X);
-// }
+function decrement_Operator() {
+    var X = 5.25; //This targets the element with id "Math6" and overwrites Click This!
+    X--;
+    document.getElementById("Math6").innerHTML = (X);
+}
 
 // window.alert(Math.random() * 100);
 window.alert(Math.pow(15, 2));
