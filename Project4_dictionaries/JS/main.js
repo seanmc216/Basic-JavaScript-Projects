d0bfe562e3449faf2a0ae9e52ed884cadf7418fc
Sean Croney <103222228+seanmc216@users.noreply.github.com>
2023-01-31 21:12:10 +0000

    Update main.js

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -3,8 +3,8 @@ function my_Dictionary() { //Creates a function called my_Dictionary, with varia
         Name: "INXS",     //These are the properties of the variable 'Band'
         Country: "Australia",
         Genre: "New_wave",
-        Formed: 1977,
-        Formed: 1987,
+        Formed: 1977,     //These 2 pairs have identical keys. 
+        Formed: 1987,     //The last read key-value paid would get read.
     };
     delete Band.Genre;    //Deletes the Genre property
     document.getElementById("Dictionary").innerHTML = Band.Genre; //Tries to output the Genre property to the element with id "Dictionary" to overwrite 'Click me!'
